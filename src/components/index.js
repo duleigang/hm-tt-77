e2481c7a51f27ed6c3029ac1555c18252a3f26b3
小杜杜 <1763170545@qq.com>
2019-08-08 16:41:21 +0800

    频道组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@
 // 注册所有components下的组件为全局组件
 
 import MyBread from '@/components/my-bread'
+import MyChannel from '@/components/my-channel'
 
 // vue插件 暴露一个对象 对象中有一个选项 install 安装的意思
 export default {
@@ -9,5 +10,6 @@ export default {
     // Vue 为全局的Vue对象
     // 安装函数 当Vue.use(使用插件) install被调用
     Vue.component(MyBread.name, MyBread)
+    Vue.component(MyChannel.name, MyChannel)
   }
 }
